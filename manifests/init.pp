f4f6bb2a0948b71bacdbade706ee561631515fef
root <root@jimmoser1c.mylabserver.com>
2020-07-10 15:31:20 +0000

    Added vhosts defined type

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,12 @@ class apache (
   String $install_ensure,
   String $config_ensure,
   String $config_path,
-  Enum["running", "stopped"] $service_ensure,
   String $service_name,
+  Enum["running", "stopped"] $service_ensure,
   Boolean $service_enable,
+  String[1] $vhosts_dir,
+  String[1] $vhosts_owner,
+  String[1] $vhosts_group,
 ) {
   contain apache::install
   contain apache::config
